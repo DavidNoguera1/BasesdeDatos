0686317bed4a6f2441f12f687210d67e6a88f732
DESKTOP-LFOCKGF\David Noguera <davidnoguera0912@gmail.com>
2023-09-04 09:51:23 -0500

    Añadiendo documentacion

--- serializacion/src/main/java/servlets/SvVideo.java
+++ serializacion/src/main/java/servlets/SvVideo.java
@@ -17,7 +17,7 @@ public class SvVideo extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-        // Asegúrate de obtener el ServletContext correctamente
+        // Nos permite asegurarnos de obtener el ServletContext correctamente
         ServletContext servletContext = getServletContext();
         
         // Cargar los videos serializados al iniciar la aplicación
@@ -30,7 +30,7 @@ public class SvVideo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Aquí vienen los datos por doPost
-        // Manda las variables por debajo del bus
+        // Manda las variables pero no las muestra por motivos de seguridad
         String idVideo = request.getParameter("idVideo");
         String titulo = request.getParameter("titulo");
         String autor = request.getParameter("autor");
@@ -38,7 +38,9 @@ public class SvVideo extends HttpServlet {
         String url = request.getParameter("url");
         String categoria = request.getParameter("categoria");
         String letra = request.getParameter("letra");   
-
+        
+        //Añadimos un try n catch donde realizamos un paseInt a la ID
+        //Tambien hay una excepcion en caso de erroes
         try {
             int idVideoInt = Integer.parseInt(idVideo);
             // Ingresar datos al objeto
@@ -50,7 +52,8 @@ public class SvVideo extends HttpServlet {
             System.out.println("Error al convertir idVideo a entero: " + e.getMessage());
         }
 
-        // Guardar la lista de videos en un archivo
+        // Guardar la lista de videos en un archivo .ser
+        // Metodod en la parte de abajo
         guardarVideosEnArchivo();
 
         // Agregar el ArrayList al objeto de solicitud
@@ -61,10 +64,13 @@ public class SvVideo extends HttpServlet {
     }
 
 
-    // Método para guardar la lista de videos en un archivo
+    // Método para guardar la lista de videos en un archivo videos.ser
     private void guardarVideosEnArchivo() {
         try {
             // Obtener la ruta real de la carpeta "data" en el proyecto web
+            // Esta esta dentro de target>serializacion-1.0>data
+            // Netbeans no tiene la capacidad de ver archivos serilizados como otras IDEs de java
+            // Entonces debe accederse al archivo por bloc de notas
             String dataPath = getServletContext().getRealPath("/data");
 
             // Verificar si la carpeta "data" existe, si no, crearla
@@ -74,6 +80,12 @@ public class SvVideo extends HttpServlet {
             }
 
             // Crear un archivo para guardar o reescribir la lista de videos serializada
+            // Notese el uso de FileOutput "fos" y ObjectOutupt "oos" para este tipo de archivos.
+            // "fos" ayuda a guardar un objeto serializable.
+            // "oos" convierte el objeto en bytes.
+            // Debemos cerrar "oos" y tambien hay una excepcion.
+            
+            
             String filePath = dataPath + File.separator + "videos.ser";
             FileOutputStream fos = new FileOutputStream(filePath);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -86,13 +98,19 @@ public class SvVideo extends HttpServlet {
         }
     }
     
-    // Método para cargar los videos desde el archivo
+    // Método para cargar los videos desde el archivo descerilizandolo
     public void cargarVideosDesdeArchivo(ServletContext servletContext) {
         try {
             // Obtener la ruta real del archivo de datos
+            // Esta esta dentro de target>serializacion-1.0>data
             String dataPath = servletContext.getRealPath("/data/videos.ser");
             
             // Verificar si el archivo existe
+            // Notece el uso de FileInput "fis" y ObjectInput "ois"
+            // "fis" nos ayuda a indicar que informacion queremos descerializar
+            // "ois" descerializa el objeto previamnete indicado y se castea.
+            // "ois" tambien debe cerrarse
+            
             File archivo = new File(dataPath);
             if (archivo.exists()) {
                 FileInputStream fis = new FileInputStream(dataPath);
